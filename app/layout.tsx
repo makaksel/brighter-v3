784c81d84e1f7f872a7268713e97f7ba63bf3b20
Alex Makarov <makarovoptionmail@gmail.com>
2023-11-17 17:49:35 +0700

    upd metrica

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Onest } from 'next/font/google';
 import './globals.css';
 import './page.scss';
 import { SvgSprite } from '@/src/shared/Svg/SvgSprite';
-import Head from 'next/head';
 import Script from 'next/script';
 
 const onest = Onest({ subsets: ['latin'] });
@@ -20,24 +19,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang='ru'>
-    <Head>
-      <Script id='yandex-metrica'>
-        {`
+    <Script id='yandex-metrica'>
+      {`
           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
           m[i].l=1*new Date();
           for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
           k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
           (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
           ym(95597975, "init", ${JSON.stringify({
-          clickmap: true,
-          trackLinks: true,
-          accurateTrackBounce: true,
-        })});
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+      })});
         `}
-      </Script>
+    </Script>
 
-      <Script>
-        {`
+    <Script>
+      {`
         var settings = {
               need: 30, 
               checkTime: 10,
@@ -262,9 +260,7 @@ export default function RootLayout({
            
           ActiveScore.init(metricsFn);
         `}
-      </Script>
-
-    </Head>
+    </Script>
 
     {/**/}
     <noscript>
