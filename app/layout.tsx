b6b81f3b512a6ef0842f1edba41911316042c008
Alex Makarov <makarovoptionmail@gmail.com>
2023-11-20 13:23:06 +0700

    upd favicon for ios

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import './page.scss';
 import { SvgSprite } from '@/src/shared/Svg/SvgSprite';
 import Script from 'next/script';
+import Head from 'next/head';
 
 const onest = Onest({ subsets: ['latin'] });
 
@@ -19,6 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang='ru'>
+    <Head>
+      <link rel='icon' href='/icon.png' sizes='32x32' />
+      <link rel='icon' href='/icon.png' sizes='192x192' />
+      <link rel='apple-touch-icon' href='/icon.png' />
+    </Head>
     <Script id='yandex-metrica'>
       {`
           (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
