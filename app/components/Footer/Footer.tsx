6c8104b1efcfe1370eccf1a300eb6e0c0970af90
Alex Makarov <makarovoptionmail@gmail.com>
2023-12-02 17:15:13 +0700

    texts

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -24,10 +24,10 @@ export const Footer: React.FC<FooterProps> = ({theme = 'dark', className}) => <f
     </div>
     <div className={cn('bottom')}>
       <div className={cn('docs')}>
-        <a href='/police.doc' className={cn('bottom-link')} download="Политика конфиденциальности">Политика конфиденциальности</a>
-        <a href='/sends.docx' className={cn('bottom-link')} download="Согласие на рассылку">Согласие на рассылку</a>
-        <a href='/operations.docx' className={cn('bottom-link')} download="Согласие на обработку ПД">Согласие на обработку ПД</a>
-        <a href='/offert.docx' className={cn('bottom-link')} download="Оферта">Оферта</a>
+        <a href='/police.doc' className={cn('bottom-text')} download="Политика конфиденциальности">Политика конфиденциальности</a>
+        <a href='/sends.docx' className={cn('bottom-text')} download="Согласие на рассылку">Согласие на рассылку</a>
+        <a href='/operations.docx' className={cn('bottom-text')} download="Согласие на обработку ПД">Согласие на обработку ПД</a>
+        <a href='/offert.docx' className={cn('bottom-text')} download="Оферта">Оферта</a>
       </div>
       <div className={cn('social')}>
         {/*<Link className={cn('social-link')} href={`#`}>in</Link>*/}
