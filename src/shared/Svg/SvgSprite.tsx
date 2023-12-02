49d93fca105b211c7d4f7e4f51f9d052e932e4ac
Alex Makarov <makarovoptionmail@gmail.com>
2023-12-02 20:22:34 +0700

    texts

--- src/shared/Svg/SvgSprite.tsx
+++ src/shared/Svg/SvgSprite.tsx
@@ -1,12 +1,10 @@
 'use client';
 
 import React, { useMemo } from 'react';
-import DOMPurify from 'isomorphic-dompurify';
 // Import a necessary function in the ES6 way (recommended):
-import { sanitize, isSupported } from "isomorphic-dompurify";
+import { sanitize } from 'isomorphic-dompurify';
 
-import { svgIcons } from "@/src/shared/Svg/SvgIcons";
-import dynamic from 'next/dynamic';
+import { svgIcons } from '@/src/shared/Svg/SvgIcons';
 
 export const SvgSprite = () => {
   // console.log(isSupported, 'isSupported')
@@ -16,47 +14,46 @@ export const SvgSprite = () => {
 
         const sanitized = sanitize(dom, {
           USE_PROFILES: { svg: true, svgFilters: true },
-          RETURN_DOM: true
+          RETURN_DOM: true,
         });
 
         // console.log(sanitized)
         return sanitized;
 
-       /* sanitized.querySelectorAll("svg").forEach((svg: SVGElement) => {
-          const attrs = Array.from(svg.attributes);
-          const symbol = document.createElement("symbol");
+        /* sanitized.querySelectorAll("svg").forEach((svg: SVGElement) => {
+           const attrs = Array.from(svg.attributes);
+           const symbol = document.createElement("symbol");
 
-          for (const attr of attrs) {
-            symbol.setAttribute(attr.nodeName, `${attr.nodeValue}`);
-          }
+           for (const attr of attrs) {
+             symbol.setAttribute(attr.nodeName, `${attr.nodeValue}`);
+           }
 
-          symbol.removeAttribute("xmlns");
-          symbol.removeAttribute("height");
-          symbol.removeAttribute("width");
+           symbol.removeAttribute("xmlns");
+           symbol.removeAttribute("height");
+           symbol.removeAttribute("width");
 
-          symbol.setAttribute("id", name);
+           symbol.setAttribute("id", name);
 
-          symbol.innerHTML = svg.innerHTML;
+           symbol.innerHTML = svg.innerHTML;
 
-          svg.replaceWith(symbol);
-        });
+           svg.replaceWith(symbol);
+         });
 
-        return sanitized.innerHTML;
-*/
+         return sanitized.innerHTML;
+ */
       })
-      .join("");
+      .join('');
 
     return result;
   }, []);
 
   return (
-    <svg style={{ display: "none" }} xmlns="http://www.w3.org/2000/svg">
+    <svg style={{ display: 'none' }} xmlns='http://www.w3.org/2000/svg'>
       <defs
         dangerouslySetInnerHTML={{
-          __html: processed
+          __html: processed,
         }}
       />
     </svg>
   );
 };
-
